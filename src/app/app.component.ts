9d76979330b9f35ebf0a1a22236acf677fc47551
nolto1sj <sjnolton@gmail.com>
2022-10-31 15:49:57 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menuitem'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-menu-lab';
+  title: string = 'Chirpus Pizza';
+
+  menuItemArray: MenuItem[] = [
+    {
+      item: "Chicken Fingers",
+      category: "dinner",
+      price: 11.99
+    },
+    {
+      item: "Pizza",
+      category: "dinner",
+      price: 11.99
+    },
+    {
+      item: "Wings",
+      category: "sides",
+      price: 8.99
+    },
+    {
+      item: "Breadsticks",
+      category: "sides",
+      price: 4.99
+    },
+    {
+      item: "Ceasar Salad",
+      category: "salads",
+      price: 5.99
+    },
+    {
+      item: "Cinnamon Roll",
+      category: "dessert",
+      price: 8.99
+    },
+  ];
+
+  returnsMenu(): MenuItem[] {
+    return this.menuItemArray;
+  }
 }
